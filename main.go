4bc8ec4486e9d8fe1f8890442a99489b38f92021
Lukas Schwab <lukas.schwab@gmail.com>
2025-10-22 14:21:02 -0700

    Serve a bookmarklet script
    The idea here is to support a "Send to pseudofeed" bookmarklet that
    works like Feedbin's, by embedding a script in the current page and
    executing it.
    
    My initial bookmarklet implementation tried to execute a POST request
    from bookmarklet Javascript, but ran into a common security restriction:
    it violates `script-src 'unsafe-inline'`.
    
    Feedbin violates `script-src [trusted-domain]`, but apparently that is
    only loosely enforced.
    
    I will revert this change if it doesn't work.

--- main.go
+++ main.go
@@ -28,9 +28,13 @@ const (
 //go:embed templates/html.tmpl
 var htmlTemplate string
 
+//go:embed templates/bookmarklet.tmpl
+var bookmarkletTemplate string
+
 var (
-	file string
-	tmpl *template.Template
+	file            string
+	tmpl            *template.Template
+	tmplBookmarklet *template.Template
 )
 
 func init() {
@@ -57,6 +61,11 @@ func init() {
 	if err != nil {
 		panic(err)
 	}
+
+	tmplBookmarklet, err = template.New("bookmarklet").Parse(bookmarkletTemplate)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
@@ -74,6 +83,22 @@ func main() {
 			})
 		}
 		return c.Send(raw)
+	})
+
+	app.Get("/bookmarklet", func(c *fiber.Ctx) error {
+		c.Response().Header.Set("Content-Type", "application/javascript")
+		host := c.Request().URI().Host()
+		type TemplateData struct {
+			BaseURL string
+		}
+		if err := tmplBookmarklet.Execute(c, TemplateData{BaseURL: string(host)}); err != nil {
+			return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
+				"error": "Error executing template",
+				"raw":   err.Error(),
+			})
+		}
+
+		return nil
 	})
 
 	app.Get("/", func(c *fiber.Ctx) error {
